8d5f99650cf357b130998ed3fe439f58d5965738
Ishita Lal <123542508+Ishitalal@users.noreply.github.com>
2025-03-10 11:24:29 +0530

    Delete job portal directory

--- job portal/url.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.job_list, name='job_list'),
-    path('job/<int:pk>/', views.job_detail, name='job_detail'),
-    path('job/post/', views.job_post, name='job_post'),
-    path('signup/', views.signup, name='signup'),
-]
